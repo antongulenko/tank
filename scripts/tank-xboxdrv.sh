9c92ef076b289cec41bc408851157f8e23896f66
Anton Gulenko <anton.gulenko@googlemail.com>
2020-10-10 23:24:32 +0200

    Made script more robust

--- scripts/tank-xboxdrv.sh
+++ scripts/tank-xboxdrv.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-xboxdrv -d |& stdbuf -i0 -o0 -e0 grep ERROR | ( read xxx; pkill -SIGKILL xboxdrv; )
+xboxdrv -d |&\
+    stdbuf -i0 -o0 -e0 grep 'USB read failure: 32: LIBUSB_TRANSFER_ERROR' |\
+    ( read && { pkill -f tank-control-daemon; pkill -SIGKILL xboxdrv; }; )
 
